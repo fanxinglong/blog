8b87958a23b825cf17c3b253cdb57c1c41292e74
fanxinglong <fxllong262553@gmail.com>
2013-05-23 23:07:20 +0800

    add rspec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+#encoding=utf-8
 module ApplicationHelper
+
+  # 根据每页的显示不同的标题
+  def full_title(page_title)
+    base_title = "个人博客"
+    if page_title.empty?  #判断一下页面标题是否为空
+      base_title  #如果为空，就返回基本标题
+    else
+      "#{base_title} | #{page_title}" #否则就是完整的标题
+    end
+  end
+
 end
